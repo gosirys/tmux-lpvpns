aed45d3bed68d0df12d2c9104388fe2fb77b821e
osirys <i@osirys.me>
2022-08-08 16:29:17 +1000

    quick commit

--- scripts/tmux-lpvpns-helper.sh
+++ scripts/tmux-lpvpns-helper.sh
@@ -13,23 +13,23 @@ tmux_get() {
     [ -n "$value" ] && echo "$value" || echo "$2"
 }
 
-get_tmux_option() {
-    local option=$1
-    local default_value=$2
-    local option_value="$(tmux show-option -gqv "$option")"
+# get_tmux_option() {
+#     local option=$1
+#     local default_value=$2
+#     local option_value="$(tmux show-option -gqv "$option")"
 
-    if [[ -z "$option_value" ]]; then
-        echo "$default_value"
-    else
-        echo "$option_value"
-    fi
-}
+#     if [[ -z "$option_value" ]]; then
+#         echo "$default_value"
+#     else
+#         echo "$option_value"
+#     fi
+# }
 
-set_tmux_option() {
-    local option=$1
-    local value=$2
-    tmux set-option -gq "$option" "$value"
-}
+# set_tmux_option() {
+#     local option=$1
+#     local value=$2
+#     tmux set-option -gq "$option" "$value"
+# }
 
 _tmux_hackon() {
 
